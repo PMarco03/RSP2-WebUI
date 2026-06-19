f2020f2d2ffcb267ef70f84f519e6426adea2ca1
MarcoPozziDeveloper <marco.pozzi03@gmail.com>
2026-06-19 23:36:46 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,17 +6,11 @@ APP_DIR="/home/RSP2-WebUI"
 
 cd "$APP_DIR"
 
-# dipendenze pulite (opzionale ma consigliato)
-rm -rf node_modules package-lock.json
+git pull
 
+rm -rf node_modules package-lock.json
 npm install
 
 npm run build
 
-# install serve se non esiste
-if ! command -v serve >/dev/null 2>&1; then
-  npm install -g serve
-fi
-
-# serve in background sulla porta 3000
-nohup serve -s dist -l 3000 > server.log 2>&1 &
+sudo systemctl reload nginx
